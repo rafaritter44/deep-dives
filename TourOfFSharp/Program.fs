ffbb904509ea40b29ee157c331cdb21ebbe49c9c
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-02-01 14:52:17 -0300

    Improve POC by using use

--- TourOfFSharp/Program.fs
+++ TourOfFSharp/Program.fs
@@ -498,7 +498,7 @@ module ImplementingInterfaces =
         member this.ReadLine() = file.ReadLine()
         interface System.IDisposable with
             member this.Dispose() = file.Close()
-    let readFile = new ReadFile()
-    let line = readFile.ReadLine()
+    let line =
+        use readFile = new ReadFile()
+        readFile.ReadLine()
     printfn $"{line}"
-    (readFile :> System.IDisposable).Dispose()
