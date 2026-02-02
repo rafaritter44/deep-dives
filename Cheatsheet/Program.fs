8d8502023c8d664681fa2262151a71ab9bf0f6ae
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-02-02 20:53:00 -0300

    Add another anonymous function POC

--- Cheatsheet/Program.fs
+++ Cheatsheet/Program.fs
@@ -5,3 +5,9 @@
         |> List.forall (fun (x, y) -> x > y)
     printfn $"{isDescending [3; 2; 1]}"
     printfn $"{isDescending [3; 2; 1; 2]}"
+
+    type Person = { Age : int }
+    let records = seq { yield { Age = 30 }; yield { Age = 150 } }
+    let suspiciousRecords =
+        records
+        |> Seq.filter (fun x -> x.Age >= 150)
