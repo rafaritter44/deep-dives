7c4fcda0ddcde61479315a0f11030218e4de1fc9
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-02-03 21:02:07 -0300

    Implement _.Property shorthand POC

--- Cheatsheet/Program.fs
+++ Cheatsheet/Program.fs
@@ -6,9 +6,19 @@
     printfn $"{isDescending [3; 2; 1]}"
     printfn $"{isDescending [3; 2; 1; 2]}"
 
-    type Person = { Age : int }
-    let records = seq { yield { Age = 30 }; yield { Age = 150 } }
+    type Person = { Name: string; Age : int }
+    let records = seq { yield { Name = "Young"; Age = 30 }; yield { Name = "Old"; Age = 150 } }
     let suspiciousRecords =
         records
         |> Seq.filter (fun x -> x.Age >= 150)
     printfn $"%A{suspiciousRecords}"
+
+    let people = Seq.toList records
+    let names =
+        people
+        |> List.map (fun person -> person.Name)
+    let names' =
+        people
+        |> List.map _.Name
+    let uppercaseNames =
+        people |> List.map _.Name.ToUpperInvariant()
