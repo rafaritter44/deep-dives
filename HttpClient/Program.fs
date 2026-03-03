cd2cf3e886a57bee4c1f91c6292a5e583a37a20a
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-03-03 13:18:21 -0300

    Implement HttpClient POC

--- HttpClient/Program.fs
+++ HttpClient/Program.fs
@@ -1,1 +1,31 @@
-﻿printfn "Hello from System.Net.Http.HttpClient POC"
+﻿open System.Net.Http
+open System.Text.Json
+open System.Threading.Tasks
+
+let client = new HttpClient()
+let baseUrl = "https://openholidaysapi.org/"
+let url path = baseUrl + path
+let jsonOptions = JsonSerializerOptions(PropertyNameCaseInsensitive = true)
+
+let get<'T> path : Task<'T> =
+    task {
+        use! response = client.GetAsync(url path)
+        response.EnsureSuccessStatusCode() |> ignore
+        use! stream = response.Content.ReadAsStreamAsync()
+        let! result = JsonSerializer.DeserializeAsync<'T>(stream, jsonOptions)
+        return result
+    }
+
+type Language = string
+type Name =
+    { Language : Language
+      Text     : string }
+type Country =
+    { IsoCode           : string
+      Name              : Name list
+      OfficialLanguages : Language list }
+
+let countries = (get<Country list> "Countries").Result
+printfn "%A" countries
+
+client.Dispose()
