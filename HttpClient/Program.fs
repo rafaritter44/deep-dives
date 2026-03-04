a4d01642b4a395dc7f7d28b08e3ccff3f9b900c5
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-03-04 11:00:15 -0300

    Add call with query params to HttpClient POC

--- HttpClient/Program.fs
+++ HttpClient/Program.fs
@@ -1,4 +1,5 @@
-﻿open System.Collections.Generic
+﻿open System
+open System.Collections.Generic
 open System.Net.Http
 open System.Text.Json
 open System.Threading.Tasks
@@ -39,5 +40,28 @@ type LanguageObject =
 
 let languages = (get<LanguageObject list> "Languages").Result
 printfn "%A" languages
+
+type Subdivision =
+    { Code      : string
+      ShortName : string }
+
+type PublicHoliday =
+    { Id            : Guid
+      Nationwide    : bool
+      Type          : string
+      RegionalScope : string
+      TemporalScope : string
+      StartDate     : DateOnly
+      EndDate       : DateOnly
+      Name          : Name list
+      Subdivisions  : Subdivision list }
+
+let queryParams = dict [
+    "countryIsoCode", "BR"
+    "languageIsoCode", "PT"
+    "validFrom", "2025-01-01"
+    "validTo", "2025-12-31" ]
+let publicHolidays = (getWithQuery<PublicHoliday list> "PublicHolidays" queryParams).Result
+printfn "%A" publicHolidays
 
 client.Dispose()
