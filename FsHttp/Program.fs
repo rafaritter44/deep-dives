311c11a0b4b8af2e8b1f4b45a37e40cdd1a25885
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-03-06 10:23:08 -0300

    Implement FsHttp POC

--- FsHttp/Program.fs
+++ FsHttp/Program.fs
@@ -1,2 +1,23 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open FsHttp
+open System.Text.Json
+
+let baseUrl = "https://openholidaysapi.org/"
+let url path = baseUrl + path
+let jsonOptions = JsonSerializerOptions(PropertyNameCaseInsensitive = true)
+
+type Language = string
+type Name =
+    { Language : Language
+      Text     : string }
+type Country =
+    { IsoCode           : string
+      Name              : Name list
+      OfficialLanguages : Language list }
+
+http {
+    GET (url "Countries")
+}
+|> Request.send
+|> Response.toJson
+|> fun json -> json.Deserialize<Country list> jsonOptions
+|> printfn "%A"
