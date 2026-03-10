2a570fe174a74e9e7fdf88b70aae887c5030433d
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-03-10 20:10:12 -0300

    Implement Dapper user table POC

--- Dapper/Program.fs
+++ Dapper/Program.fs
@@ -13,3 +13,28 @@ let testConnection () =
     conn.QuerySingle<int> "SELECT 1"
 
 testConnection() |> printfn "Result: %d"
+
+let createTable () =
+    use conn = getConnection()
+    let sql = """
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL
+        )
+    """
+    conn.Execute sql |> ignore
+
+createTable()
+
+let insertUser name =
+    use conn = getConnection()
+    let sql = "INSERT INTO users (name) VALUES (@name) RETURNING id"
+    conn.QuerySingle<int>(sql, {| name = name |})
+
+insertUser "Rafael" |> printfn "Inserted user %d"
+
+let getUsers () =
+    use conn = getConnection()
+    conn.Query<string> "SELECT name FROM users"
+
+getUsers() |> printfn "%A"
