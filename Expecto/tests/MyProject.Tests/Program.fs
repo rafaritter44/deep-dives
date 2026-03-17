508a55ed22642993d6e52e25f20ab6a0dec5942c
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-03-17 13:17:47 -0300

    Implement Expecto test case

--- Expecto/tests/MyProject.Tests/Program.fs
+++ Expecto/tests/MyProject.Tests/Program.fs
@@ -1,14 +1,20 @@
 ﻿open Expecto
 
 let tests =
-  testList "A test list" [
-    test "A test" {
-        let actual = 2+2
-        let expected = 4
-        let message = "2+2 = 4"
-        Expect.equal actual expected message
-    }
-  ]
+    testList "A test list" [
+        test "A test" {
+            let actual = 2+2
+            let expected = 4
+            let message = "2+2 = 4"
+            Expect.equal actual expected message
+        }
+
+        testCase "A test case" <| fun () ->
+            let actual = 2+2
+            let expected = 4
+            let message = "2+2 = 4"
+            Expect.equal actual expected message
+    ]
 
 [<EntryPoint>]
 let main args =
