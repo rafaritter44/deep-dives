be51555f0560d455d327b9436ad690aa36c7d3f4
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-03-18 12:16:53 -0300

    Remove duplication from Expecto POC

--- Expecto/tests/MyProject.Tests/Program.fs
+++ Expecto/tests/MyProject.Tests/Program.fs
@@ -1,28 +1,22 @@
 ﻿open Expecto
 
 let tests =
+    let expected = 4
+    let message = "2+2 = 4"
     testList "A test list" [
         test "A test" {
             let actual = 2+2
-            let expected = 4
-            let message = "2+2 = 4"
             Expect.equal actual expected message
         }
         testCase "A test case" <| fun () ->
             let actual = 2+2
-            let expected = 4
-            let message = "2+2 = 4"
             Expect.equal actual expected message
         testAsync "An async test" {
             let! actual = async { return 2+2 }
-            let expected = 4
-            let message = "2+2 = 4"
             Expect.equal actual expected message
         }
         testTask "A task test" {
             let! actual = task { return 2+2 }
-            let expected = 4
-            let message = "2+2 = 4"
             Expect.equal actual expected message
         }
     ]
