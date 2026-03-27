7249da44404529ade89c69efb6fd2695c6c8975f
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-03-27 11:57:51 -0300

    Enhance get-started POC once more

--- get-started/hello.hs
+++ get-started/hello.hs
@@ -1,5 +1,7 @@
 main = do
   print (6 + 3^2 * 4)
-  print (take 10 (filter even [43..]))
+  let xs = take 10 (filter even [43..])
+  print xs
+  print (sum xs)
   putStrLn "Hello, everybody!"
   putStrLn ("Please look at my favorite odd numbers: " ++ show (filter odd [10..20]))
