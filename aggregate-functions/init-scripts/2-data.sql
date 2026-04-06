1c88fc555e3358b4a76a35c1c0ff754759dee9e8
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-06 17:27:48 -0300

    Simplify init-scripts for aggregate functions POC

--- aggregate-functions/init-scripts/2-data.sql
+++ aggregate-functions/init-scripts/2-data.sql
@@ -1,26 +1,11 @@
-with new_product as (
-    insert into product (name, brand, size, price)
-    values ('T-Shirt', 'Foo', 'L', 12.50),
-           ('T-Shirt', 'Bar', 'L', 12.00),
-           ('T-Shirt', 'Bar', 'M', 9.50),
-           ('Pants', 'Foo', 'L', 12.50)
-    returning product_id
-)
-insert into sale (product_id, units)
-select product_id, 5
-from new_product;
-
-with new_product as (
-    insert into product (name, brand, size, price)
-    values ('T-Shirt', 'Foo', 'M', 10.00),
-           ('Pants', 'Foo', 'M', 10.00),
-           ('Pants', 'Bar', 'M', 9.50)
-    returning product_id
-)
-insert into sale (product_id, units)
-select product_id, 10
-from new_product;
-
-insert into product (name, brand, size, price)
-values ('Pants', 'Bar', 'L', 12.00),
-       ('Pants', 'Baz', 'L', 17.50);
+insert into test (str, num)
+values ('A', -1),
+       ('A', 0),
+       ('A', 1),
+       ('A', 2),
+       ('B', 3),
+       ('B', 4),
+       ('B', 5),
+       ('B', null),
+       (null, 10),
+       (null, null);
