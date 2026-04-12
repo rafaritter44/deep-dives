2e5fe577ca507a086d0eb87ce424205f9068e1c9
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-12 17:36:01 -0300

    Implement from subquery POC

--- subqueries/init-scripts/2-data.sql
+++ subqueries/init-scripts/2-data.sql
@@ -4,6 +4,10 @@ values (1, 'Engineering'),
        (3, 'HR');
 
 insert into employee (e_no, e_name, salary, d_no)
-values (1, 'Alice', 2000, 1),
-       (2, 'Bob', 1500, 2),
-       (3, 'Charlie', 1000, 3);
+values (1, 'Alice',   7000, 1),
+       (2, 'Bob',     6000, 1),
+       (3, 'Charlie', 5000, 1),
+       (4, 'David',   4000, 2),
+       (5, 'Eve',     3000, 2),
+       (6, 'Frank',   2000, 3),
+       (7, 'Grace',   1000, 3);
