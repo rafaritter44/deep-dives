ad56cf856e66fa33ac9a2c622c49cbf755747e47
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-22 13:01:52 -0300

    Implement some code from CIS 194 lecture 2

--- cis-194/02-adts.hs
+++ cis-194/02-adts.hs
@@ -4,12 +4,22 @@ data Thing = Shoe
            | Cabbage
            | King
     deriving Show
+
 shoe :: Thing
 shoe = Shoe
+
 listO'Things :: [Thing]
 listO'Things = [Shoe, SealingWax, King, Cabbage, King]
+
+isSmall :: Thing -> Bool
+isSmall Shoe       = True
+isSmall Ship       = False
+isSmall SealingWax = True
+isSmall Cabbage    = True
+isSmall King       = False
 
 main :: IO ()
 main = do
     print shoe
     print listO'Things
+    print (map isSmall listO'Things)
