298f16619ee5e8b78faf50dabb672cd80c7a22b0
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-24 13:55:55 -0300

    Implement some code from CIS 194 lecture 3

--- cis-194/03-rec-poly.hs
+++ cis-194/03-rec-poly.hs
@@ -8,6 +8,13 @@ mapIntList :: (Int -> Int) -> IntList -> IntList
 mapIntList _ Empty       = Empty
 mapIntList f (Cons x xs) = Cons (f x) (mapIntList f xs)
 
+filterIntList :: (Int -> Bool) -> IntList -> IntList
+filterIntList _ Empty       = Empty
+filterIntList p (Cons x xs)
+    | p x       = Cons x (filterIntList p xs)
+    | otherwise = filterIntList p xs
+
 main :: IO ()
 main = do
     print (mapIntList abs intList)
+    print (filterIntList even intList)
