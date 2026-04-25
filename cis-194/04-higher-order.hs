eb5000b6477d14a97f870ef6b810580ff628302e
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-25 18:54:08 -0300

    Implement some code from CIS 194 lecture 4

--- cis-194/04-higher-order.hs
+++ cis-194/04-higher-order.hs
@@ -7,9 +7,14 @@ greaterThan100 xs = filter g100 xs
 greaterThan100_2 :: [Integer] -> [Integer]
 greaterThan100_2 xs = filter (\x -> x > 100) xs
 
+greaterThan100_3 :: [Integer] -> [Integer]
+greaterThan100_3 xs = filter (>100) xs
+
 main :: IO ()
 main = do
     let integers :: [Integer]
         integers = [1,9,349,6,907,98,105]
     print $ greaterThan100 integers
     print $ greaterThan100_2 integers
+    print $ (\x y z -> [x,2*y,3*z]) 5 6 3
+    print $ greaterThan100_3 integers
