002875c5b2a61056e162124ccabb8cba47df1333
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-25 14:29:08 -0300

    Implement some code from CIS 194 lecture 3

--- cis-194/03-rec-poly.hs
+++ cis-194/03-rec-poly.hs
@@ -14,7 +14,20 @@ filterIntList p (Cons x xs)
     | p x       = Cons x (filterIntList p xs)
     | otherwise = filterIntList p xs
 
+data List t = E | C t (List t)
+    deriving Show
+
+lst1 :: List Int
+lst1 = C 3 (C 5 (C 2 E))
+
+lst2 :: List Char
+lst2 = C 'x' (C 'y' (C 'z' E))
+
+lst3 :: List Bool
+lst3 = C True (C False E)
+
 main :: IO ()
 main = do
     print (mapIntList abs intList)
     print (filterIntList even intList)
+    print (lst1, lst2, lst3)
