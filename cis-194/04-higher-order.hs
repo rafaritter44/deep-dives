91740ed759d31f061ef4fe8b7e89f9a2a3be10c1
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-25 18:57:21 -0300

    Implement some code from CIS 194 lecture 4

--- cis-194/04-higher-order.hs
+++ cis-194/04-higher-order.hs
@@ -18,3 +18,6 @@ main = do
     print $ greaterThan100_2 integers
     print $ (\x y z -> [x,2*y,3*z]) 5 6 3
     print $ greaterThan100_3 integers
+    print $ (>100) 102
+    print $ (100>) 102
+    print $ map (*6) [1..5]
