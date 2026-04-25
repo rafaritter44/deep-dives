3cb4dc73470046ddc276f5703c40940389c76cb9
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-25 18:48:22 -0300

    Implement some code from CIS 194 lecture 4

--- cis-194/04-higher-order.hs
+++ cis-194/04-higher-order.hs
@@ -4,8 +4,12 @@ g100 x = x > 100
 greaterThan100 :: [Integer] -> [Integer]
 greaterThan100 xs = filter g100 xs
 
+greaterThan100_2 :: [Integer] -> [Integer]
+greaterThan100_2 xs = filter (\x -> x > 100) xs
+
 main :: IO ()
 main = do
     let integers :: [Integer]
         integers = [1,9,349,6,907,98,105]
     print $ greaterThan100 integers
+    print $ greaterThan100_2 integers
