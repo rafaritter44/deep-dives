ffb4b3d3fc0ab916a14c3d22a80f5849c9ca69d0
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-27 13:07:32 -0300

    Implement some code from CIS 194 lecture 5

---
+++ cis-194/05-type-classes.hs
@@ -0,0 +1,24 @@
+data Foo = F Int | G Char
+
+instance Eq Foo where
+    (F i1) == (F i2) = i1 == i2
+    (G c1) == (G c2) = c1 == c2
+    _ == _ = False
+
+    foo1 /= foo2 = not (foo1 == foo2)
+
+f1 :: Foo
+f1 = F 1
+
+f2 :: Foo
+f2 = F 2
+
+f3 :: Foo
+f3 = G 'a'
+
+f4 :: Foo
+f4 = G 'b'
+
+main :: IO ()
+main = do
+    print $ f1 == f1 && f1 /= f2 && f2 /= f3 && f3 /= f4 && f4 == f4
