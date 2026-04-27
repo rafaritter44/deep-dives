33b776f61d83622ec7c0f9f267ec1a7ce2b0b0a8
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-04-27 13:23:46 -0300

    Implement some code from CIS 194 lecture 5

--- cis-194/05-type-classes.hs
+++ cis-194/05-type-classes.hs
@@ -39,3 +39,4 @@ main = do
     print $ f1 == f1 && f1 /= f2 && f2 /= f3 && f3 /= f4 && f4 == f4
     print $ f1' == f1' && f1' /= f2' && f2' /= f3' && f3' /= f4' && f4' == f4'
     print [f1', f2', f3', f4']
+    print $ f1' < f2' && f2' < f3' && f3' < f4'
