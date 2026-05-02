9d89c549f4a288f6a6402d15c32558a467926ad8
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-02 18:40:43 -0300

    Implement some code from CIS 194 lecture 8

--- cis-194/08-io.hs
+++ cis-194/08-io.hs
@@ -1,2 +1,9 @@
+data D = C String Integer Bool
+    deriving Show
+
+d :: D
+d = C "abc" 123 True
+
 main :: IO ()
 main = putStrLn "Please enter a number: " >> (readLn >>= (\(n :: Integer) -> print (n+1)))
+    >> print d
