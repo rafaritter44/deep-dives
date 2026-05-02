fa4f51facf907338c7523f4e0740cabab88a5b30
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-02 18:42:31 -0300

    Implement some code from CIS 194 lecture 8

--- cis-194/08-io.hs
+++ cis-194/08-io.hs
@@ -4,6 +4,13 @@ data D = C String Integer Bool
 d :: D
 d = C "abc" 123 True
 
+data D' = C' { field1 :: String, field2 :: Integer, field3 :: Bool }
+    deriving Show
+
+d' :: D'
+d' = C' { field3 = True, field2 = 123, field1 = "abc" }
+
 main :: IO ()
 main = putStrLn "Please enter a number: " >> (readLn >>= (\(n :: Integer) -> print (n+1)))
     >> print d
+    >> print d'
