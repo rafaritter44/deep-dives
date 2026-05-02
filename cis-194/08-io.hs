52fdaa6c5b08e2ef261c4165d85c918ddca75690
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-02 18:43:32 -0300

    Implement some code from CIS 194 lecture 8

--- cis-194/08-io.hs
+++ cis-194/08-io.hs
@@ -10,7 +10,11 @@ data D' = C' { field1 :: String, field2 :: Integer, field3 :: Bool }
 d' :: D'
 d' = C' { field3 = True, field2 = 123, field1 = "abc" }
 
+d'' :: D'
+d'' = C' "abc" 123 True
+
 main :: IO ()
 main = putStrLn "Please enter a number: " >> (readLn >>= (\(n :: Integer) -> print (n+1)))
     >> print d
     >> print d'
+    >> print d''
