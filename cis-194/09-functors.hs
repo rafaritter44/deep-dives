e95669b7cf7f4d869bafae199429731976030405
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-03 11:09:45 -0300

    Implement some code from CIS 194 lecture 9

--- cis-194/09-functors.hs
+++ cis-194/09-functors.hs
@@ -4,6 +4,14 @@ data Funny f a = Funny a (f a)
 funny :: Funny Maybe Int
 funny = Funny 1 (Just 2)
 
+class Functor f where
+  fmap :: (a -> b) -> f a -> f b
+
+instance Main.Functor Maybe where
+    fmap _ Nothing  = Nothing
+    fmap h (Just a) = Just (h a)
+
 main :: IO ()
 main = do
     print funny
+    print $ Main.fmap succ (Just (1 :: Int))
