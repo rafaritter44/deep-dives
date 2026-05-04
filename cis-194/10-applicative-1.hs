46372446f14988f8bc679f9ecfc065992bd6b84e
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-04 11:14:46 -0300

    Implement some code from CIS 194 lecture 10

--- cis-194/10-applicative-1.hs
+++ cis-194/10-applicative-1.hs
@@ -8,6 +8,11 @@ maybeEmployee :: Maybe Name -> Maybe String -> Maybe Employee
 maybeEmployee (Just name) (Just phone) = Just (Employee name phone)
 maybeEmployee _ _                      = Nothing
 
+employeeList :: [Name] -> [String] -> [Employee]
+employeeList (x:xs) (y:ys) = Employee x y : employeeList xs ys
+employeeList _ _           = []
+
 main :: IO ()
 main = do
     print $ zipWith maybeEmployee [Just "Alice", Just "Bob", Nothing] [Just "+5548999999999", Nothing, Just "+5511999999999"]
+    print $ employeeList ["Alice", "Bob", "Charlie"] ["+5548999999999", "+5511999999999"]
