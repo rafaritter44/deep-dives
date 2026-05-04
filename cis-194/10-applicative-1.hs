3a5d85c7ac67a9d65ab36da94d18f0c2cbf7f1b1
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-04 11:19:24 -0300

    Implement some code from CIS 194 lecture 10

--- cis-194/10-applicative-1.hs
+++ cis-194/10-applicative-1.hs
@@ -12,7 +12,20 @@ employeeList :: [Name] -> [String] -> [Employee]
 employeeList (x:xs) (y:ys) = Employee x y : employeeList xs ys
 employeeList _ _           = []
 
+data E = E Name String
+    deriving Show
+
+getName :: E -> Name
+getName (E n _) = n
+
+getPhone :: E -> String
+getPhone (E _ p) = p
+
+getEmployee :: E -> Employee
+getEmployee e = Employee (getName e) (getPhone e)
+
 main :: IO ()
 main = do
     print $ zipWith maybeEmployee [Just "Alice", Just "Bob", Nothing] [Just "+5548999999999", Nothing, Just "+5511999999999"]
     print $ employeeList ["Alice", "Bob", "Charlie"] ["+5548999999999", "+5511999999999"]
+    print $ getEmployee (E "Alice" "+5548999999999")
