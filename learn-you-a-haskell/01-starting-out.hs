e818a49ef0ac5980259731f8c87e5755b4a9d235
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-13 16:30:38 -0300

    Implement some code from Learn You a Haskell's Starting Out chapter

--- learn-you-a-haskell/01-starting-out.hs
+++ learn-you-a-haskell/01-starting-out.hs
@@ -5,3 +5,8 @@ main = do
     print $ 'A' : " SMALL CAT"
     print (5 : [1,2,3,4,5] :: [Integer])
     print $ "0123456789" !! 7
+    print $ [3,2,1] > [2,1,0]
+    print $ [3,2,1] > [2,10,100]
+    print $ [3,4,2] > [3,4]
+    print $ [3,4,2] > [2,4]
+    print $ [3,4,2] == [3,4,2]
