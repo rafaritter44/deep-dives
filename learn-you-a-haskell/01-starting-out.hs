55eed3dfc4c53384ffce293b506883eb30924b86
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-13 16:25:04 -0300

    Implement some code from Learn You a Haskell's Starting Out chapter

--- learn-you-a-haskell/01-starting-out.hs
+++ learn-you-a-haskell/01-starting-out.hs
@@ -2,3 +2,5 @@ main :: IO ()
 main = do
     print ([1,2,3,4] ++ [9,10,11,12] :: [Integer])
     print $ "hello" ++ " " ++ "world"
+    print $ 'A' : " SMALL CAT"
+    print (5 : [1,2,3,4,5] :: [Integer])
