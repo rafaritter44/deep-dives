4dcea73a0a385b349abfadc588917c0d580b4a86
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-14 14:04:04 -0300

    Implement some code from Learn You a Haskell's Starting Out chapter

--- learn-you-a-haskell/01-starting-out.hs
+++ learn-you-a-haskell/01-starting-out.hs
@@ -55,3 +55,5 @@ main = do
     let removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
     print $ removeNonUppercase "Hahaha! Ahahaha!"
     print $ removeNonUppercase "IdontLIKEFROGS"
+    let xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]
+    print [[x | x <- xs, even x ] | xs <- xxs]
