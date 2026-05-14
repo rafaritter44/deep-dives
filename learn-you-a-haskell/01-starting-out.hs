29fd045a5d8b358acbb59c364e3d93d363fa4c3d
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-14 15:12:29 -0300

    Implement some code from Learn You a Haskell's Starting Out chapter

--- learn-you-a-haskell/01-starting-out.hs
+++ learn-you-a-haskell/01-starting-out.hs
@@ -71,3 +71,4 @@ main = do
     print $ zip [1,2,3,4,5] [5,5,5,5,5]
     print $ zip [1..5] ["one", "two", "three", "four", "five"]
     print $ zip [5,3,2,6,2,7,2,5,4,6,6] ["im","a","turtle"]
+    print $ zip [1..] ["apple", "orange", "cherry", "mango"]
