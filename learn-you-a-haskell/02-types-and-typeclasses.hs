913347f98d4ba366da13ec234e8245f9f77177f7
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-14 20:18:09 -0300

    Implement some code from Learn You a Haskell's Types and Typeclasses chapter

--- learn-you-a-haskell/02-types-and-typeclasses.hs
+++ learn-you-a-haskell/02-types-and-typeclasses.hs
@@ -4,3 +4,6 @@ factorial n = product [1..n]
 main :: IO ()
 main = do
     print $ factorial 50
+    print $ "Abrakadabra" < "Zebra"
+    print $ "Abrakadabra" `compare` "Zebra"
+    print $ 5 `compare` 3
