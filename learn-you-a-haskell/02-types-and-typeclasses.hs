a7bd7378ffbc56f2dce75f7927da92bc30ef641d
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-15 17:16:16 -0300

    Implement some code from Learn You a Haskell's Types and Typeclasses chapter

--- learn-you-a-haskell/02-types-and-typeclasses.hs
+++ learn-you-a-haskell/02-types-and-typeclasses.hs
@@ -4,12 +4,18 @@ factorial n = product [1..n]
 main :: IO ()
 main = do
     print $ factorial 50
+
+    -- Ord
     print $ "Abrakadabra" < "Zebra"
     print $ "Abrakadabra" `compare` "Zebra"
     print $ 5 `compare` 3
+
+    -- Show
     print $ show 3
     print $ show 5.334
     print $ show True
+
+    -- Read
     print $ read "True" || False
     print $ read "8.2" + 3.8
     print $ read "5" - 2
@@ -19,7 +25,11 @@ main = do
     print $ (read "5" :: Float) * 4
     print (read "[1,2,3,4]" :: [Int])
     print (read "(3, 'a')" :: (Int, Char))
+
+    -- Enum
     print (['a'..'e'], [LT .. GT], [3..5], succ 'B')
+
+    -- Bounded
     print (minBound :: Int)
     print (maxBound :: Char)
     print (maxBound :: Bool)
