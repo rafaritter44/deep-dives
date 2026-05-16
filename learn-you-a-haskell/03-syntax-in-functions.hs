d3885c669b48b2019b5ea85ecb4e7bbd2e9dd5c8
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-16 20:47:01 -0300

    Implement some code from Learn You a Haskell's Syntax in Functions chapter

--- learn-you-a-haskell/03-syntax-in-functions.hs
+++ learn-you-a-haskell/03-syntax-in-functions.hs
@@ -2,6 +2,15 @@ lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky _ = "Sorry, you're out of luck, pal!"
 
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe _ = "Not between 1 and 5"
+
 main :: IO ()
 main = do
     print $ map lucky ([3, 7, 12] :: [Int])
+    print $ map sayMe ([1..6] :: [Int])
