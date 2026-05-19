39b57d46e8a5aad01c713be64abf5f51d528b181
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-19 12:03:10 -0300

    Implement some code from Learn You a Haskell's Syntax in Functions chapter

--- learn-you-a-haskell/03-syntax-in-functions.hs
+++ learn-you-a-haskell/03-syntax-in-functions.hs
@@ -51,6 +51,12 @@ capital :: String -> String
 capital "" = "Empty string, whoops!"
 capital wholeStr@(x:_) = "The first letter of " ++ wholeStr ++ " is " ++ [x]
 
+densityTell :: (RealFloat a) => a -> String
+densityTell density
+    | density < 1.2     = "Wow! You're going for a ride in the sky!"
+    | density <= 1000.0 = "Have fun swimming, but watch out for sharks!"
+    | otherwise         = "If it's sink or swim, you're going to sink."
+
 main :: IO ()
 main = do
     print $ map lucky ([3, 7, 12] :: [Int])
@@ -65,3 +71,4 @@ main = do
     print (length' "Hello" :: Int)
     print (sum' [1,2,3,4,5] :: Int)
     print $ capital "Hello"
+    print $ densityTell (1000 :: Float)
