0a7ab9c572dd2b748c11130e093a071f3ccbd019
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-21 12:03:35 -0300

    Implement some code from Learn You a Haskell's Syntax in Functions chapter

--- learn-you-a-haskell/03-syntax-in-functions.hs
+++ learn-you-a-haskell/03-syntax-in-functions.hs
@@ -140,3 +140,4 @@ main = do
     print $ 4 * (let a = 9 in a + 1) + (2 :: Int)
     print $ let square (x :: Int) = x * x in (square 5, square 3, square 2)
     print (let (a :: Int) = 100; b = 200; c = 300 in a*b*c, let foo="Hey "; bar = "there!" in foo ++ bar)
+    print $ (let (a,b,c) = (1,2,3) in a+b+c) * (100 :: Int)
