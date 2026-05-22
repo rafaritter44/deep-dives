bf38fd5b5688ee360d92d94ce07ebdd93f41a257
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-22 15:21:35 -0300

    Implement some code from Learn You a Haskell's Recursion chapter

---
+++ learn-you-a-haskell/04-recursion.hs
@@ -0,0 +1,11 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+main :: IO ()
+main = do
+    print $ maximum' ([1,2,3,4,50,6,70,8,9,10] :: [Integer])
