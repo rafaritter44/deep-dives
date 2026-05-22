c31739a4e82a160be8105892c1b08d7d182aa778
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-22 15:08:39 -0300

    Implement some code from Learn You a Haskell's Syntax in Functions chapter

--- learn-you-a-haskell/03-syntax-in-functions.hs
+++ learn-you-a-haskell/03-syntax-in-functions.hs
@@ -127,6 +127,11 @@ head''' :: [a] -> a
 head''' xs = case xs of [] -> error "No head for empty lists!"
                         (x:_) -> x
 
+describeList :: [a] -> String
+describeList xs = "The list is " ++ case xs of [] -> "empty."
+                                               [x] -> "a singleton list."
+                                               xs -> "a longer list."
+
 main :: IO ()
 main = do
     print $ map lucky ([3, 7, 12] :: [Int])
@@ -162,3 +167,4 @@ main = do
     let boot (x :: Int) y z = x * y + z in print $ boot 3 4 2
     print $ head'' "Hello"
     print $ head''' "Hello"
+    print $ map describeList ["", "a", "abc"]
