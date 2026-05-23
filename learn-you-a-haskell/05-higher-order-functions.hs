60d78abc5ba565d1f0323cbb1696f0a1010428d6
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-23 11:14:20 -0300

    Implement some code from Learn You a Haskell's Higher-Order Functions chapter

---
+++ learn-you-a-haskell/05-higher-order-functions.hs
@@ -0,0 +1,17 @@
+-- Curried functions
+
+-- Higher-order functions
+
+-- Maps and filters
+
+-- Lambdas
+
+-- Folds
+
+-- Function application with $
+
+-- Function composition
+
+main :: IO ()
+main = do
+    print ""
