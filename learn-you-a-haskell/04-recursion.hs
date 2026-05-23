0c997e0f289322cceaaf0892f2c716f1e9e41e31
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-23 10:55:29 -0300

    Implement some code from Learn You a Haskell's Recursion chapter

--- learn-you-a-haskell/04-recursion.hs
+++ learn-you-a-haskell/04-recursion.hs
@@ -16,8 +16,15 @@ replicate' n x
     | n <= 0    = []
     | otherwise = x:replicate' (n-1) x
 
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0   = []
+take' _ []     = []
+take' n (x:xs) = x : take' (n-1) xs
+
 main :: IO ()
 main = do
     print $ maximum' ([1,2,3,4,50,6,70,8,9,10] :: [Int])
     print $ maximum'' ([1,2,3,4,50,6,70,8,9,10] :: [Int])
     print $ replicate' (10 :: Int) "abc"
+    print $ take' (3 :: Int) "abcdef"
