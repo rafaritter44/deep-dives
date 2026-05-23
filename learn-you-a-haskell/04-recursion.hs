69b5daa98734d3a4335361c3e679ecb9cfd09e38
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-23 10:52:27 -0300

    Implement some code from Learn You a Haskell's Recursion chapter

--- learn-you-a-haskell/04-recursion.hs
+++ learn-you-a-haskell/04-recursion.hs
@@ -11,7 +11,13 @@ maximum'' [] = error "maximum of empty list"
 maximum'' [x] = x
 maximum'' (x:xs) = max x (maximum' xs)
 
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
 main :: IO ()
 main = do
     print $ maximum' ([1,2,3,4,50,6,70,8,9,10] :: [Int])
     print $ maximum'' ([1,2,3,4,50,6,70,8,9,10] :: [Int])
+    print $ replicate' (10 :: Int) "abc"
