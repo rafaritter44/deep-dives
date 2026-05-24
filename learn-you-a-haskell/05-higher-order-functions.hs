c7bf1c902db92a154398f57ef25044fd8662cc13
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-24 16:57:07 -0300

    Implement some code from Learn You a Haskell's Higher-Order Functions chapter

--- learn-you-a-haskell/05-higher-order-functions.hs
+++ learn-you-a-haskell/05-higher-order-functions.hs
@@ -1,5 +1,11 @@
 -- Curried functions
 
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+multTwoWithNine :: Int -> Int -> Int
+multTwoWithNine = multThree 9
+
 -- Higher-order functions
 
 -- Maps and filters
@@ -14,4 +20,18 @@
 
 main :: IO ()
 main = do
+    -- Curried functions
     print $ (max 4) (5 :: Int)
+    print $ multTwoWithNine 2 3
+
+    -- Higher-order functions
+
+    -- Maps and filters
+
+    -- Lambdas
+
+    -- Folds
+
+    -- Function application with $
+
+    -- Function composition
