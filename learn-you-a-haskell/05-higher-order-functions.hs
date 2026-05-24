a60c278f02aad6db5c26ef1cf89ccb75b9e0b76e
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-24 16:57:46 -0300

    Implement some code from Learn You a Haskell's Higher-Order Functions chapter

--- learn-you-a-haskell/05-higher-order-functions.hs
+++ learn-you-a-haskell/05-higher-order-functions.hs
@@ -6,6 +6,9 @@ multThree x y z = x * y * z
 multTwoWithNine :: Int -> Int -> Int
 multTwoWithNine = multThree 9
 
+multWithEighteen :: Int -> Int
+multWithEighteen = multTwoWithNine 2
+
 -- Higher-order functions
 
 -- Maps and filters
@@ -23,6 +26,7 @@ main = do
     -- Curried functions
     print $ (max 4) (5 :: Int)
     print $ multTwoWithNine 2 3
+    print $ multWithEighteen 10
 
     -- Higher-order functions
 
