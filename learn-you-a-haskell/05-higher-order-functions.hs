05858f9e403a2346198c3de5e85f4b65201f290a
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-24 16:58:41 -0300

    Implement some code from Learn You a Haskell's Higher-Order Functions chapter

--- learn-you-a-haskell/05-higher-order-functions.hs
+++ learn-you-a-haskell/05-higher-order-functions.hs
@@ -9,6 +9,9 @@ multTwoWithNine = multThree 9
 multWithEighteen :: Int -> Int
 multWithEighteen = multTwoWithNine 2
 
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred x = compare 100 x
+
 -- Higher-order functions
 
 -- Maps and filters
@@ -27,6 +30,7 @@ main = do
     print $ (max 4) (5 :: Int)
     print $ multTwoWithNine 2 3
     print $ multWithEighteen 10
+    print $ compareWithHundred (99 :: Int)
 
     -- Higher-order functions
 
