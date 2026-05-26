02b6fe5252e090561cb2d3bcaa7b732ce39c9996
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-26 11:21:25 -0300

    Implement some code from Learn You a Haskell's Higher-Order Functions chapter

--- learn-you-a-haskell/05-higher-order-functions.hs
+++ learn-you-a-haskell/05-higher-order-functions.hs
@@ -26,6 +26,11 @@ isUpperAlphanum = (`elem` ['A'..'Z'])
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
 
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
 -- Maps and filters
 
 -- Lambdas
@@ -53,6 +58,11 @@ main = do
     print $ applyTwice ("HAHA " ++) "HEY"
     print $ applyTwice (multThree 2 2) (9 :: Int)
     print $ applyTwice (3:) [1 :: Int]
+    print $ zipWith' (+) [4,2,5,6] ([2,6,2,3] :: [Int])
+    print $ zipWith' max [6,3,2,1] ([7,3,1,5] :: [Int])
+    print $ zipWith' (++) ["a-", "b-", "c-"] ["1", "2", "3"]
+    print $ zipWith' (*) (replicate 5 2) ([1..] :: [Int])
+    print $ zipWith' (zipWith' (*)) [[1,2,3],[3,5,6],[2,3,4]] ([[3,2,2],[3,4,5],[5,4,3]] :: [[Int]])
 
     -- Maps and filters
 
