4e017f7401606e3c6d334a7699966e4b5b5cc2b8
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-28 17:27:40 -0300

    Implement some code from Learn You a Haskell's Higher-Order Functions chapter

--- learn-you-a-haskell/05-higher-order-functions.hs
+++ learn-you-a-haskell/05-higher-order-functions.hs
@@ -67,6 +67,10 @@ chain n
     | even n = n:chain (n `div` 2)
     | odd n  = n:chain (n*3 + 1)
 
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
+
 -- Lambdas
 
 -- Folds
@@ -120,6 +124,7 @@ main = do
     print (sum (takeWhile (<10000) (filter odd (map (^(2 :: Int)) [1..]))) :: Int)
     print (sum (takeWhile (<10000) [n^(2 :: Int) | n <- [1..], odd (n^(2 :: Int))]) :: Int)
     print (chain 10, chain 30)
+    print numLongChains
 
     -- Lambdas
 
