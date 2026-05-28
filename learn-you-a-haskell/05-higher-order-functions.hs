0037f5828e800b2e947392a80714b23a4901f3f0
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-28 17:34:03 -0300

    Implement some code from Learn You a Haskell's Higher-Order Functions chapter

--- learn-you-a-haskell/05-higher-order-functions.hs
+++ learn-you-a-haskell/05-higher-order-functions.hs
@@ -64,8 +64,8 @@ largestDivisible = head (filter p [100000,99999..])
 chain :: (Integral a) => a -> [a]
 chain 1 = [1]
 chain n
-    | even n = n:chain (n `div` 2)
-    | odd n  = n:chain (n*3 + 1)
+    | even n    = n:chain (n `div` 2)
+    | otherwise = n:chain (n*3 + 1)
 
 numLongChains :: Int
 numLongChains = length (filter isLong (map chain [1..100]))
