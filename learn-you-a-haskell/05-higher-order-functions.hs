bc2da8215af279eb076c84324844b42226d8f886
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-05-29 11:33:29 -0300

    Implement some code from Learn You a Haskell's Higher-Order Functions chapter

--- learn-you-a-haskell/05-higher-order-functions.hs
+++ learn-you-a-haskell/05-higher-order-functions.hs
@@ -134,6 +134,7 @@ main = do
     -- Lambdas
     print numLongChains'
     print (zipWith (\a b -> (a * 30 + 3) / b) [5,4,3,2,1] [1,2,3,4,5] :: [Float])
+    print (map (\(a,b) -> a + b) [(1,2),(3,5),(6,3),(2,6),(2,5)] :: [Int])
 
     -- Folds
 
