ecd498341245bd7b908fac4b8c1d053f539efae2
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-06-03 15:12:12 -0300

    Implement some code from Learn You a Haskell's Higher-Order Functions chapter

--- learn-you-a-haskell/05-higher-order-functions.hs
+++ learn-you-a-haskell/05-higher-order-functions.hs
@@ -224,3 +224,5 @@ main = do
     print (negate . (*3) $ 10 :: Int)
     print (map (\x -> negate (abs x)) [5,-3,-6,7,-3,2,-19,24] :: [Int])
     print (map (negate . abs) [5,-3,-6,7,-3,2,-19,24] :: [Int])
+    print (map (\xs -> negate (sum (tail xs))) [[1..5],[3..6],[1..7]] :: [Int])
+    print (map (negate . sum . tail) [[1..5],[3..6],[1..7]] :: [Int])
