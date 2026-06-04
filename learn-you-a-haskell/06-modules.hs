c8d49ad23574aea11e4d7f0e03ed3a612657a82e
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-06-04 13:11:25 -0300

    Implement some code from Learn You a Haskell's Modules chapter

--- learn-you-a-haskell/06-modules.hs
+++ learn-you-a-haskell/06-modules.hs
@@ -1,10 +1,15 @@
 import Data.List
 -- import Data.List (nub, sort)
 -- import Data.List hiding (nub)
+import qualified Data.Map
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
 
+intStrList :: [(Int, String)]
+intStrList = [(5,"a"), (3,"b")] :: [(Int, String)]
+
 main :: IO ()
 main = do
     print $ numUniques "aaabbbcccddd"
+    print $ Data.Map.filter (> "a") $ Data.Map.fromList intStrList
