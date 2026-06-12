d0d1d30f89a24d053147262b2f8ce2a46737c810
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-06-12 16:11:50 -0300

    Implement some code from Learn You a Haskell's Modules chapter

--- learn-you-a-haskell/06-modules.hs
+++ learn-you-a-haskell/06-modules.hs
@@ -22,3 +22,4 @@ main = do
     print (transpose [[1,2,3],[4,5,6],[7,8,9]] :: [[Int]])
     print $ transpose ["hey", "there", "folks"]
     print (map sum $ transpose [[0,3,5,9],[10,0,0,9],[8,5,1,-1]] :: [Int])
+    print $ concat ["foo","bar","car"]
