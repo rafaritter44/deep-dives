6acbc5a70d3f8f2588447b5a7f2878474e5dbb29
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-06-15 16:14:50 -0300

    Implement some code from Learn You a Haskell's Modules chapter

--- learn-you-a-haskell/06-modules.hs
+++ learn-you-a-haskell/06-modules.hs
@@ -28,3 +28,4 @@ main = do
     print $ and $ map (>4) ([5,6,7,8] :: [Int])
     print $ and $ map (==4) ([4,4,4,3,4] :: [Int])
     print $ or $ map (==4) ([2,3,4,5,6,1] :: [Int])
+    print $ or $ map (>4) ([1,2,3] :: [Int])
