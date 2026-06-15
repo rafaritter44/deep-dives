765a4a4ae98824f55c3e82724b7a9c0963d47f7f
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-06-15 16:14:26 -0300

    Implement some code from Learn You a Haskell's Modules chapter

--- learn-you-a-haskell/06-modules.hs
+++ learn-you-a-haskell/06-modules.hs
@@ -27,3 +27,4 @@ main = do
     print (concatMap (replicate 4) [1..3] :: [Int])
     print $ and $ map (>4) ([5,6,7,8] :: [Int])
     print $ and $ map (==4) ([4,4,4,3,4] :: [Int])
+    print $ or $ map (==4) ([2,3,4,5,6,1] :: [Int])
