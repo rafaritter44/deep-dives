2c123f7e4766b22c9a3449cce639a035038dd039
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-06-17 10:01:33 -0300

    Add partitions to range-partitioned table

--- partitioning/init-scripts/1-schema.sql
+++ partitioning/init-scripts/1-schema.sql
@@ -1,3 +1,4 @@
+-- Range Partitioning
 create table order (
     id          bigserial      primary key,
     customer_id bigint         not null,
@@ -5,5 +6,13 @@ create table order (
     status      text           not null,
     created_at  timestamptz    not null
 ) partition by range (created_at);
-
 create index order_created_at_idx on order (created_at);
+create table order_2026_01
+    partition of order
+    for values from ('2026-01-01') to ('2026-02-01');
+create table order_2026_02
+    partition of order
+    for values from ('2026-02-01') to ('2026-03-01');
+create table order_2026_03
+    partition of order
+    for values from ('2026-03-01') to ('2026-04-01');
