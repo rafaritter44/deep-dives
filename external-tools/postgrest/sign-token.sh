0fbca84cdd52347a51cb2b67009b5350a9319f53
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-06-23 12:48:24 -0300

    Add expiration to PostgREST JWT

--- external-tools/postgrest/sign-token.sh
+++ external-tools/postgrest/sign-token.sh
@@ -3,10 +3,12 @@ set -e
 
 JWT_SECRET='reallyreallyreallyreallyverysafe'
 
+exp=$(( $(date +%s) + 5*60 )) # five minutes
+
 _base64 () { openssl base64 -e -A | tr '+/' '-_' | tr -d '='; }
 
 header=$(echo -n '{"alg":"HS256","typ":"JWT"}' | _base64)
-payload=$(echo -n "{\"role\":\"todo_user\"}" | _base64)
+payload=$(echo -n "{\"role\":\"todo_user\",\"exp\":$exp}" | _base64)
 signature=$(echo -n "$header.$payload" | openssl dgst -sha256 -hmac "$JWT_SECRET" -binary | _base64)
 
 echo -n "$header.$payload.$signature"
