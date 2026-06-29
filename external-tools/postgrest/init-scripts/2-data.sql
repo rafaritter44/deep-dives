bf1b5a9040051039ad4051025e0dde6bdfbb1573
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-06-29 18:28:21 -0300

    Implement PostgREST <img> POC

--- external-tools/postgrest/init-scripts/2-data.sql
+++ external-tools/postgrest/init-scripts/2-data.sql
@@ -1,2 +1,7 @@
 insert into api.todo (task)
-values ('finish tutorial 0'), ('pat self on back');
+values ('finish tutorial 0'),
+       ('pat self on back');
+
+insert into api.file (blob)
+values (pg_read_binary_file('/images/postgres.webp')),
+       (pg_read_binary_file('/images/ted-codd.webp'));
