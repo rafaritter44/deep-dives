bc0f9918e1ffea265ecbca08b79c60b4d6c31499
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-07-03 11:25:00 -0300

    Make PostgREST <img> POC more generic

--- external-tools/postgrest/init-scripts/2-data.sql
+++ external-tools/postgrest/init-scripts/2-data.sql
@@ -2,6 +2,6 @@ insert into api.todo (task)
 values ('finish tutorial 0'),
        ('pat self on back');
 
-insert into api.file (blob)
-values (pg_read_binary_file('/images/postgres.webp')),
-       (pg_read_binary_file('/images/ted-codd.webp'));
+insert into api.file (name, blob)
+values ('postgres.webp', pg_read_binary_file('/images/postgres.webp')),
+       ('ted-codd.webp', pg_read_binary_file('/images/ted-codd.webp'));
