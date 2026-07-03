32433fe4580f2fe543c48956475e24be249e8308
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-07-03 11:30:21 -0300

    Use two files with different extensions in PostgREST <img> POC

--- external-tools/postgrest/init-scripts/2-data.sql
+++ external-tools/postgrest/init-scripts/2-data.sql
@@ -3,5 +3,5 @@ values ('finish tutorial 0'),
        ('pat self on back');
 
 insert into api.file (name, blob)
-values ('postgres.webp', pg_read_binary_file('/images/postgres.webp')),
+values ('postgres.png',  pg_read_binary_file('/images/postgres.png')),
        ('ted-codd.webp', pg_read_binary_file('/images/ted-codd.webp'));
