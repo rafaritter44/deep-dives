2804e9af1fc5068af3ca7af667bf39c7605e1f91
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-07-30 14:45:28 -0300

    Implement peer comparison self-join POC

---
+++ features/joins/queries/self-peer-comparison.sql
@@ -0,0 +1,11 @@
+select e1.ein        as ein,
+       e1.manager_no as manager_no,
+       e1.e_no       as e1_no,
+       e1.e_name     as e1_name,
+       e2.e_no       as e2_no,
+       e2.e_name     as e2_name
+from employee e1
+join employee e2
+    on  e1.ein        = e2.ein
+    and e1.manager_no = e2.manager_no
+    and e1.e_no       < e2.e_no;
