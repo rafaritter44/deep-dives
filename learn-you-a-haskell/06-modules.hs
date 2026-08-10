6256fb499b9d3f1f722fa05a62992a0b01c1103c
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-10 17:20:49 -0300

    Implement some code from Learn You a Haskell's Modules chapter

--- learn-you-a-haskell/06-modules.hs
+++ learn-you-a-haskell/06-modules.hs
@@ -42,3 +42,5 @@ main = do
     print $ takeWhile (>3) ([6,5,4,3,2,1,2,3,4,5,4,3,2,1] :: [Int])
     print $ takeWhile (/=' ') "This is a sentence"
     print $ sum $ takeWhile (<10000) $ map (^ (3 :: Int)) ([1..] :: [Int])
+    print $ dropWhile (/=' ') "This is a sentence"
+    print $ dropWhile (<3) ([1,2,2,2,3,4,5,4,3,2,1] :: [Int])
