e2ff776d45dbf9f8b86b2c9965470075d35e5bc7
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-10 17:19:45 -0300

    Implement some code from Learn You a Haskell's Modules chapter

--- learn-you-a-haskell/06-modules.hs
+++ learn-you-a-haskell/06-modules.hs
@@ -41,3 +41,4 @@ main = do
     print $ let (a,b) = splitAt 3 "foobar" in b ++ a
     print $ takeWhile (>3) ([6,5,4,3,2,1,2,3,4,5,4,3,2,1] :: [Int])
     print $ takeWhile (/=' ') "This is a sentence"
+    print $ sum $ takeWhile (<10000) $ map (^ (3 :: Int)) ([1..] :: [Int])
