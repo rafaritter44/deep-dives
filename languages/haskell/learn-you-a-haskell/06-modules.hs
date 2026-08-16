9566fd2b7cbaf61b508ba723e773c6e115bb3497
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-16 15:09:47 -0300

    Implement some code from Learn You a Haskell's Modules chapter

--- languages/haskell/learn-you-a-haskell/06-modules.hs
+++ languages/haskell/learn-you-a-haskell/06-modules.hs
@@ -76,3 +76,5 @@ main = do
     print $ findIndex (==4) ([5,3,2,1,6,4] :: [Int])
     print $ findIndex (==7) ([5,3,2,1,6,4] :: [Int])
     print $ findIndices (`elem` ['A'..'Z']) "Where Are The Caps?"
+    print (zipWith3 (\x y z -> x + y + z) [1,2,3] [4,5,2,2] [2,2,3] :: [Int])
+    print (zip4 [2,3,3] [2,2,2] [5,5,3] [2,2,2] :: [(Int, Int, Int, Int)])
