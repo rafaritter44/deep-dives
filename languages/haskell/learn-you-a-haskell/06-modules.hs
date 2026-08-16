209f0abdd3d6bc4b1b9d9f054ed741ef8d04afd4
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-16 15:11:27 -0300

    Implement some code from Learn You a Haskell's Modules chapter

--- languages/haskell/learn-you-a-haskell/06-modules.hs
+++ languages/haskell/learn-you-a-haskell/06-modules.hs
@@ -79,3 +79,4 @@ main = do
     print (zipWith3 (\x y z -> x + y + z) [1,2,3] [4,5,2,2] [2,2,3] :: [Int])
     print (zip4 [2,3,3] [2,2,2] [5,5,3] [2,2,2] :: [(Int, Int, Int, Int)])
     print $ lines "first line\nsecond line\nthird line"
+    print $ unlines ["first line", "second line", "third line"]
