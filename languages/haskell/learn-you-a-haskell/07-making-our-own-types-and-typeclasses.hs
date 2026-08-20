f9237ca782b29ab1cd12c7e552b67a72d40ea6f6
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-20 14:34:31 -0300

    Create skeleton for Learn You a Haskell's Making Our Own Types and Typeclasses chapter

---
+++ languages/haskell/learn-you-a-haskell/07-making-our-own-types-and-typeclasses.hs
@@ -0,0 +1,51 @@
+-- Algebraic data types
+
+-- Record syntax
+
+-- Type params
+
+-- Derived instances
+
+-- Type synonyms
+
+-- Recursive data structures
+
+-- Typeclasses 102
+
+-- A yes-no typeclass
+
+-- The Functor typeclass
+
+-- Kinds
+
+main :: IO ()
+main = do
+    -- Algebraic data types
+    putStr ""
+
+    -- Record syntax
+    putStr ""
+
+    -- Type params
+    putStr ""
+
+    -- Derived instances
+    putStr ""
+
+    -- Type synonyms
+    putStr ""
+
+    -- Recursive data structures
+    putStr ""
+
+    -- Typeclasses 102
+    putStr ""
+
+    -- A yes-no typeclass
+    putStr ""
+
+    -- The Functor typeclass
+    putStr ""
+
+    -- Kinds
+    putStr ""
