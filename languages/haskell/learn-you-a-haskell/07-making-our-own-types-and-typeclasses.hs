2e5e6c3ac66005fd0b9e0668802564f48592413b
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-23 18:41:01 -0300

    Implement some code from Learn You a Haskell's Making Our Own Types and Typeclasses chapter

--- languages/haskell/learn-you-a-haskell/07-making-our-own-types-and-typeclasses.hs
+++ languages/haskell/learn-you-a-haskell/07-making-our-own-types-and-typeclasses.hs
@@ -4,7 +4,7 @@ data Shape = Circle Float Float Float | Rectangle Float Float Float Float
 
 surface :: Shape -> Float
 surface (Circle _ _ r) = pi * r ^ (2 :: Int)
-surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface (Rectangle x1 y1 x2 y2) = abs (x2 - x1) * abs (y2 - y1)
 
 -- Record syntax
 
