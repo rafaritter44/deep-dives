b92777719d98f29a96fb5360ee5e10c4510b3de3
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-24 20:20:10 -0300

    Add data for Date/Time Types POC

--- systems/postgresql/features/data-types/init-scripts/2-data.sql
+++ systems/postgresql/features/data-types/init-scripts/2-data.sql
@@ -1,5 +1,6 @@
 insert into test (d, t, ttz, ts, tstz, i)
-values ('1999-01-08', '04:05:06.789', '04:05:06.789-8', '1999-01-08 04:05:06.789', '1999-01-08 04:05:06.789-8', ''),
+values ('1999-01-08', '04:05:06.789', '04:05:06.789-8', '1999-01-08 04:05:06.789', '1999-01-08 04:05:06.789-8', '2 years 15 months 100 weeks 99 hours 123456789 milliseconds'),
+       (null, null, null, null, null, '2 years 15 months 100 weeks 99 hours 123456789 milliseconds ago')
        (epoch, null, null, epoch, epoch, null),
        (infinity, null, null, infinity, infinity, infinity),
        (-infinity, null, null, -infinity, -infinity, -infinity),
