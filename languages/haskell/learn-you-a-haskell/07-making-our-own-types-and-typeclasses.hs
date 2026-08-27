da87d8c42fbe67d912d0e9fee9fbf533f32df332
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-08-27 20:53:46 -0300

    Implement some code from Learn You a Haskell's Making Our Own Types and Typeclasses chapter

--- languages/haskell/learn-you-a-haskell/07-making-our-own-types-and-typeclasses.hs
+++ languages/haskell/learn-you-a-haskell/07-making-our-own-types-and-typeclasses.hs
@@ -13,6 +13,10 @@ surface' :: Shape' -> Float
 surface' (Circle' _ r) = pi * r ^ (2 :: Int)
 surface' (Rectangle' (Point x1 y1) (Point x2 y2)) = abs (x2 - x1) * abs (y2 - y1)
 
+nudge :: Shape' -> Float -> Float -> Shape'
+nudge (Circle' (Point x y) r) a b = Circle' (Point (x+a) (y+b)) r
+nudge (Rectangle' (Point x1 y1) (Point x2 y2)) a b = Rectangle' (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+
 -- Record syntax
 
 -- Type params
@@ -41,6 +45,7 @@ main = do
     print $ map (Circle 10 20) [4,5,6,6]
     print $ surface' $ Rectangle' (Point 0 0) (Point 100 100)
     print $ surface' $ Circle' (Point 0 0) 24
+    print $ nudge (Circle' (Point 34 34) 10) 5 10
 
     -- Record syntax
     putStr ""
