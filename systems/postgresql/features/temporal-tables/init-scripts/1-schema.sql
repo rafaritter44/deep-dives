3b02ff32fb0f38902b32e2b957b87db5b5c98059
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-09-08 14:31:05 -0300

    Improve schema for PostgreSQL Temporal Tables example

--- systems/postgresql/features/temporal-tables/init-scripts/1-schema.sql
+++ systems/postgresql/features/temporal-tables/init-scripts/1-schema.sql
@@ -1,16 +1,17 @@
-CREATE TABLE products (
+create table product (
     product_no integer,
-    price numeric,
-    valid_at daterange,
-    PRIMARY KEY (product_no, valid_at WITHOUT OVERLAPS)
-    -- UNIQUE (product_no, valid_at WITHOUT OVERLAPS)
+    price      numeric,
+    valid_at   daterange,
+    primary key (product_no, valid_at without overlaps)
+    -- unique (product_no, valid_at without overlaps)
 );
 
-CREATE TABLE variants (
-  id         integer,
+create table variant (
+  variant_id integer,
   product_no integer,
   name       text,
   valid_at   daterange,
-  PRIMARY KEY (id, valid_at WITHOUT OVERLAPS),
-  FOREIGN KEY (product_no, PERIOD valid_at) REFERENCES products (product_no, PERIOD valid_at)
+  primary key (variant_id, valid_at without overlaps),
+  foreign key (product_no, period valid_at) references product (product_no, period valid_at)
+  -- foreign key (product_no, period valid_at) references product
 );
